4a1a9c1d5f290132d2e870ba3ce084831be6c73b
Zenlzb <leezenbeen@u.nus.edu>
2022-03-09 15:19:46 +0800

    Initial commit

---
+++ PocketShop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PocketShop
+//
+//  Created by Lee Zen Been on 9/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PocketShop/PocketShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketShopApp.swift
+//  PocketShop
+//
+//  Created by Lee Zen Been on 9/3/22.
+//
+
+import SwiftUI
+
+@main
+struct PocketShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
